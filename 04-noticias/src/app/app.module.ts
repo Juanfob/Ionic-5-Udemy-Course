de47f76a954e8a449975cf1c3181bd671dc18509
Juan Fermin <juanfob.dev@gmail.com>
2020-07-28 10:58:41 +0200

    Terminada seccion 8 - Noticias. Hasta clase 122 incluida

--- 04-noticias/src/app/app.module.ts
+++ 04-noticias/src/app/app.module.ts
@@ -15,6 +15,8 @@ import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
 import { IonicStorageModule } from '@ionic/storage';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 
 
@@ -30,7 +32,8 @@ import { IonicStorageModule } from '@ionic/storage';
     IonicStorageModule.forRoot({
       name: '_noticiasDB',
       driverOrder: ['indexeddb', 'sqlite', 'websql']
-    })
+    }),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [
     StatusBar,
--- 04-noticias/src/app/components/noticia/noticia.component.ts
+++ 04-noticias/src/app/components/noticia/noticia.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Article } from '../../interfaces/interfaces';
 
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
-import { ActionSheetController } from '@ionic/angular';
+import { ActionSheetController, Platform } from '@ionic/angular';
 
 import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 import { DataLocalService } from '../../services/data-local.service';
@@ -24,7 +24,8 @@ export class NoticiaComponent implements OnInit {
   constructor( private iab: InAppBrowser,
                public actionSheetCtrl:  ActionSheetController,
                private socialSharing: SocialSharing,
-               private dataLocalService: DataLocalService ) { }
+               private dataLocalService: DataLocalService,
+               private platform: Platform ) { }
 
   ngOnInit() {}
 
@@ -68,12 +69,10 @@ export class NoticiaComponent implements OnInit {
           cssClass: 'action-dark',
           handler: () => {
           console.log('Share clicked');
-          this.socialSharing.share( 
-            this.noticia.title,
-            this.noticia.source.name,
-            '',
-            this.noticia.url
-          );
+
+          this.compartirNoticia();
+
+          
         }
       }, 
       guardarBorrarBtn,
@@ -88,6 +87,40 @@ export class NoticiaComponent implements OnInit {
       }]
     });
     await actionSheet.present();
+  }
+
+  /**
+   * en este método, determinamos que plataforma usamos para implementar el botón de compartir
+   */
+  compartirNoticia() {
+
+    if ( this.platform.is('cordova')){
+      this.socialSharing.share( 
+        this.noticia.title,
+        this.noticia.source.name,
+        '',
+        this.noticia.url
+      );
+    } else {
+      /**
+       * En caso contrario usamos el share de developers.google.com
+       */
+      if (navigator.share) {
+        navigator.share({
+          title: this.noticia.title,
+          text: this.noticia.description,
+          url: this.noticia.url,
+        })
+          .then(() => console.log('Successful share'))
+          .catch((error) => console.log('Error sharing', error));
+      } else {
+        /**
+         *  en caso de que el navegador no soporte el share, mostramos lo que venga aqui
+         */
+        console.log('No se pudo compartir porque no es soportado por el navegador');
+      }
+    }
+    
   }
 
 }
