de0fff8edab3f8976355315d6ff2ca3f39d5e4a8
Juan Fermin <juanfob.dev@gmail.com>
2021-11-23 20:15:49 +0100

    Sección 11 - QRScanner - lección 168 finalizada

--- qrscanner/src/app/app.module.ts
+++ qrscanner/src/app/app.module.ts
@@ -6,12 +6,15 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+  BarcodeScanner],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- qrscanner/src/app/pages/tab1/tab1.page.ts
+++ qrscanner/src/app/pages/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,22 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  swiperOpts =  {
+    allowSlidePrev: false,
+    allowSlideNext: false
+  };
 
+  constructor( private barcodeScanner: BarcodeScanner) {}
+
+  scan() {
+    this.barcodeScanner.scan().then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+     }).catch(err => {
+         console.log('Error', err);
+     });
+  }
+
+  ionViewWillEnter(){
+    this.scan();
+  }
 }
